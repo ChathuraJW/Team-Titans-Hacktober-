674116fc59ee13c3ba91e082e1085be2fecce4ef
Chathura Wanniarachchi <wachathuraj@gmail.com>
2019-10-21 15:19:21 +0530

    create basic struct for doubly link list

---
+++ Doubly Link List using C/Doublay link list using C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//create linklist node  structure using struct
+struct linkListNode{
+	struct linkListNode *previous;
+	int data;
+	struct linkListNode *next;
+};
+typedef struct linkListNode *node;
+
+
+int main(){
+	
+	return 0;
+}
