2ce7ec4143d985e7d1a51c4e12a40d6f2f7dac0a
Chathura Wanniarachchi <wachathuraj@gmail.com>
2019-10-21 15:41:51 +0530

    create delete node function for DLL and optimize code by giving come comments

--- Doubly Link List using C/Doublay link list using C.c
+++ Doubly Link List using C/Doublay link list using C.c
@@ -12,6 +12,10 @@ typedef struct linkListNode *node;
 node head=NULL;
 node start=NULL;
 
+addNode(int);
+deleteNode(int);
+node createNode();
+
 //code for create node
 node createNode(){
 	node newNode;
@@ -32,7 +36,27 @@ addNode(int data){
 	head=myNode;
 }
 
+//code for delete a given node
+//it may be 1st or last or any
+deleteNode(int data){
+	node temp=start;
+	while(temp->data!=data){
+		temp=temp->next;
+	}
+	if(temp==start){
+		start=start->next;	
+		start->previous=NULL;
+	}else if(temp->next==NULL){
+		temp->previous->next=NULL;
+	}else{
+		temp->previous->next=temp->next;
+		temp->next->previous=temp->previous;
+	}
+	free(temp);
+} 
 int main(){
-	
+	//call hear above function..
+	//addNode(int) this function for create and add a node to linklist..
+	//deleteNode(int) this function for delate any given node...
 	return 0;
 }
